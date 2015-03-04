22ff9dc88b5dccdfe5d0449266aebfd1a6f5004a
ejongg <ejcampos08@gmail.com>
2015-03-04 21:55:37 +0800

    Initial commit

---
+++ js/controllers/TrucksController.js
@@ -0,0 +1,36 @@
+angular.module('Client')
+    .controller("TrucksCtrl", ['$http', '$log', '$scope', function($http, $log, $scope){
+        $scope.trucks = [];
+        
+        $scope.getTrucks = function(){
+            io.socket.get('/trucks');
+            
+            $http.get(io.sails.url + '/trucks')
+                .success(function(data){
+                    $scope.trucks = data;
+                });
+        };
+        
+        $scope.getTrucks();
+        
+        io.socket.on('trucks', function(msg){
+            switch (msg.verb){
+                case 'created' :
+                    $scope.trucks.push(msg.data);
+                    $scope.$digest();
+                    break;
+            }
+        });
+        
+        $scope.addTruck = function(){
+            var truck = {
+                driver : $scope.driver,
+                dispatcher : $scope.dispatcher,
+                agent : $scope.agent,
+                helper : $scope.helper,
+                route : $scope.route
+            };
+            
+            io.socket.post('/trucks', truck);
+        };
+    }])
